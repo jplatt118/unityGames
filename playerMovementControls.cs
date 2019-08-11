bebc9ac6ffa33137d9521a977301e6923da228e0
jplatt118 <35509919+jplatt118@users.noreply.github.com>
2019-08-11 19:06:38 -0400

    Add files via upload

---
+++ playerMovementControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementControls : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void fixedUpdate() {
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+	}
+}
